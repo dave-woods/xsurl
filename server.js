3b347dded027721dbc17a06d3fdbd641b47d1532
Dave Woods <dwoodscs@gmail.com>
2016-07-30 09:24:39 +0000

    Getting original URL

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const validUrl = require('valid-url');
 
 app.get('/', function(req, res) {
-	const resObj = {original_url: null, short_url: null};
-    
-	res.json(resObj);
+	res.send('Add a new URL!');
+});
+
+app.get('/n/*', function(req, res) {
+    console.log(req.params);
+    const original = req.params[0];
+    if (validUrl.isWebUri(original))
+    {
+    	const resObj = {original_url: original, short_url: null};
+                
+    	res.json(resObj);
+    }
+    else
+    {
+        res.json({
+            error: "That didn't look like a valid URL."
+        });
+    }
 });
 
 app.listen(port, function() {
